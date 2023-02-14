570957edebe3f30d8776780082d54871c537e9e4
hyebeen byun <hyebeen2658@naver.com>
2023-02-14 14:04:43 +0900

    learn. 클래스의 읽기전용, 읽기쓰기 메서드 작성법

--- frontend/public/learn-class-concept.js
+++ frontend/public/learn-class-concept.js
@@ -12,26 +12,37 @@ function generateUniqueId() {
   return `euid-${key}`;
 }
 
-// 클래스 컴포넌트
+//* 클래스 컴포넌트
 class Event {
   // 비공개 (private) 필드
   // 접근 제어자
   // # = class 내부에서만 접근 가능
   // 사용자가 내부에서 임의로 바꾸는것을 막기 위해서 사용
   #uid = "";
+  #type;
+  #date;
 
-  // constructor(생성자)
+  // 생성자(constructor)
   constructor(type, date) {
-    // this는 생성된 클래스의 instance(객체)를 말함.
-    this.type = type;
-    this.date = date;
+    // this는 생성된 Event 클래스의 인스턴스(객체)를 말함
+    this.#type = type;
     this.#uid = generateUniqueId();
+    this.#date = date;
   }
 
-  // instance : 생성된 객체
+  // 인스턴스(instance) : 생성된 객체
   // 생성된 객체만 사용 가능한 메서드
+  // 인스턴스 메서드
   getType() {
-    return this.type;
+    return this.#type;
+  }
+
+  getDate() {
+    return this.#date;
+  }
+
+  setDate(newDate) {
+    this.#date = newDate;
   }
 
   getUid() {
