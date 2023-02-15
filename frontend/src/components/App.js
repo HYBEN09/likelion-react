55a3b72bbbb23abb69c5df07e2debd493ca1cdf9
hyebeen byun <hyebeen2658@naver.com>
2023-02-15 16:04:02 +0900

    learn. 조건부 렌더링 vs. 조건부 표시

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,33 +1,54 @@
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      headline: "React Application",
-      isDisabled: false,
-    };
+  state = {
+    headline: "React Application",
+    // isDisabled: false,
+    isToggle: false,
+    isLoading: !true,
+    hasError: null, // { message: '서버에서 적절하지 않은 요청이 있었다는 응답이 있었습니다.' },
+  };
 
-    this.handleChangeHeadline = this.handleChangeHeadline.bind(this);
-  }
+  originalHeadline = this.state.headline;
+  willUpdateHeadline = "NEW HEADLINE! 😃";
 
-  handleChangeHeadline() {
-    this.setState({
-      headline: "New HeadLine💥",
-      isDisabled: true,
-    });
-  }
+  // 화살표 함수를 쓴 이유 : this가 class field를 가리켜야 하니까
+  handleChangeHeadline = () => {
+    // 조건 처리
+    // 문을 사용할 것인가?
+    if (this.state.isToggle) {
+      this.setState({
+        isToggle: false,
+        headline: this.originalHeadline,
+        hasError: null,
+      });
+    } else {
+      this.setState({
+        isToggle: true,
+        headline: this.willUpdateHeadline,
+      });
+    }
+
+    // this.setState({
+    //   headline: "New HeadLine💥",
+    //   isDisabled: true,
+    // });
+  };
 
   render() {
-    const { headline, isDisabled } = this.state;
+    const { isToggle, headline, isLoading, hasError } = this.state;
+
+    if (isLoading) {
+      return <div role="alert">데이터 로딩 중...</div>;
+    }
+
+    if (hasError) {
+      return <div role="alert"> {hasError.message}</div>;
+    }
 
     return (
       <div data-component="App">
         <h1>{headline}</h1>
-        <button
-          type="button"
-          onClick={this.handleChangeHeadline}
-          disabled={isDisabled}
-        >
-          헤드라인 변경
+        <button type="button" onClick={this.handleChangeHeadline}>
+          {isToggle ? "오리지널 헤드라인 으로 변경" : "뉴 헤드라인 으로 변경"}
         </button>
       </div>
     );
