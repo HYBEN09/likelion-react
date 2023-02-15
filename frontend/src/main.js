112e0c888959cc0ddd8745695b5636d6dc74bc95
hyebeen byun <hyebeen2658@naver.com>
2023-02-15 10:45:53 +0900

    config. Babel 구성
    - @babel/core
    - @babel/cli
    - @babel/preset-env

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,37 +1,5 @@
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      headline: "React Application",
-    };
-    // this가 undefined로 출력되는 문제 해결
-    this.handleChangeHeadline = this.handleChangeHeadline.bind(this);
-  }
+const App = () => console.log("callReact");
 
-  handleChangeHeadline() {
-    this.setState({
-      headline: "New HeadLine💥",
-    });
-  }
-
-  render() {
-    return (
-      <div data-component="App">
-        <h1>{this.state.headline}</h1>
-        <button type="button" onClick={this.handleChangeHeadline}>
-          헤드라인 변경
-        </button>
-      </div>
-    );
-  }
+function createElement(type, props, ...children) {
+  return { type, props: { ...props, children } };
 }
-
-/* -------------------------------------------------------------------------- */
-
-const reactDomRoot = ReactDOM.createRoot(document.getElementById("root"));
-
-reactDomRoot.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
