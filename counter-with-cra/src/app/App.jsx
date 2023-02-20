03eb35b88e21165b138d7400c8d8cef46aa78ad8
hyebeen byun <hyebeen2658@naver.com>
2023-02-20 16:17:38 +0900

    learn. 카운터 클래스 컴포넌트

---
+++ counter-with-cra/src/app/App.jsx
@@ -0,0 +1,12 @@
+import Counter from "../components/Counter";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter count={5} />
+    </div>
+  );
+}
+
+export default App;
