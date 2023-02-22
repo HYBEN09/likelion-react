fd0e8f09c14eb4152ded16afb4f247afd4471fc3
hyebeen byun <hyebeen2658@naver.com>
2023-02-22 15:59:35 +0900

    button component 만들기

---
+++ stateless-component/src/components/Button/Button.jsx
@@ -0,0 +1,38 @@
+import classes from "./Button.module.css";
+import { tokens } from "@/theme/tokens";
+
+const { colors } = tokens;
+
+export function Button({ mode, ...restProps /* { children, ... } */ }) {
+  // props로부터 파생된 지역 변수 (렌더링 과는 아무 상관 없음)
+  let isPrimary = mode.includes("primary");
+
+  return (
+    <button
+      type="button"
+      className={classes.component}
+      style={{
+        backgroundColor: isPrimary
+          ? colors.primary["500"]
+          : colors.primary["50"],
+
+        color: isPrimary ? colors.white : colors.primary["400"],
+      }}
+      {...restProps}
+    />
+  );
+}
+
+Button.defaultProps = {
+  mode: "primary", // 'secondary'
+};
+
+//* --------------------------------------------------------------------------
+
+// export function Button({ mode = 'primary', children }) {
+//   return (
+//     <button type="button" className={classes.component}>
+//       {children}
+//     </button>
+//   );
+// }
