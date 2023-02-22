fdbf475ec2bc42cf22cd21d10c9f0783c03b126a
hyebeen byun <hyebeen2658@naver.com>
2023-02-23 08:55:52 +0900

    - button component 구성 - vite 절대경로 구성 - A11yHidden 코드 정리

--- stateless-component/src/App.jsx
+++ stateless-component/src/App.jsx
@@ -1,5 +1,5 @@
 import classes from "./App.module.css";
-import { Button } from "./components";
+import { A11yHidden, Button } from "./components/index";
 
 function App() {
   const handleClick = (e) => {
@@ -7,13 +7,16 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className={classes.container}>
       <h2 className={classes.headline}>Button 컴포넌트(stateless)</h2>
+
       <div role="group" className={classes.buttonGroup}>
-        <Button onClick={handleClick}>회원가입</Button>
-        <Button mode="secondary" onClick={handleClick}>
-          로그인
-        </Button>
+        <Button>회원가입</Button>
+        <Button secondary>로그인</Button>
+
+        <A11yHidden as="h2">저장</A11yHidden>
+        <A11yHidden as="a">저장</A11yHidden>
+        <A11yHidden as="figcaption">저장</A11yHidden>
       </div>
     </div>
   );
