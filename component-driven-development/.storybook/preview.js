7e4caf370928e953c77662c34076e35f40324303
hyebeen byun <hyebeen2658@naver.com>
2023-02-27 11:26:57 +0900

    learn. Storybook 에센셜 애드온 Controls 제어 - 문서 감추기 - 컴포넌트 이름 변경(한글화)

--- component-driven-development/.storybook/preview.js
+++ component-driven-development/.storybook/preview.js
@@ -1,12 +1,14 @@
 export const parameters = {
+  controls: { expanded: true },
+
   backgrounds: {
     default: 'light',
   },
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
--- component-driven-development/src/components/Logo/Logo.stories.jsx
+++ component-driven-development/src/components/Logo/Logo.stories.jsx
@@ -5,11 +5,21 @@ export default {
   component: Logo,
   tags: ['autodocs'],
   args: Logo.defaultProps,
+  argTypes: {
+    // https://storybook.js.org/docs/react/essentials/controls#disable-controls-for-specific-properties
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
 export const Base = {
   args: {},
 };
+
+Base.storyName = '기본 로고';
 
 export const Rotation90 = {
   args: {
