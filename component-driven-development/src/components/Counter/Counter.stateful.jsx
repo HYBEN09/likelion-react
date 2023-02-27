248a5e3d6299395e48d4c2c76e610bd0c34fd63f
hyebeen byun <hyebeen2658@naver.com>
2023-02-27 15:49:46 +0900

    usememo 사용

--- component-driven-development/src/components/Counter/Counter.stateful.jsx
+++ component-driven-development/src/components/Counter/Counter.stateful.jsx
@@ -1,7 +1,11 @@
 import classes from './Counter.module.scss';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useMemo } from 'react';
+import { useCompareProp } from '@/hooks/useCompareProp';
+
+// [React hook] useMemo를 사용해 컴포넌트 기억하기
+// [React HOC] React.memo를 사용해 컴포넌트 기억하기
 
 export function CounterStateful({
   count: initialCount,
@@ -23,33 +27,47 @@ export function CounterStateful({
     setCount((count) => count - step);
   }, [step]);
 
-  return (
-    <div className={combineClassNames}>
+  const memoIncButton = useMemo(
+    () => (
       <CountButton label={buttonLabels.increment} onUpdate={handleIncrement}>
         +
       </CountButton>
+    ),
+    [buttonLabels.increment, handleIncrement]
+  );
 
-      <CountOutput>{count}</CountOutput>
-
+  const memoDecButton = useMemo(
+    () => (
       <CountButton label={buttonLabels.decrement} onUpdate={handleDecrement}>
         -
       </CountButton>
+    ),
+    [buttonLabels.decrement, handleDecrement]
+  );
+
+  return (
+    <div className={combineClassNames}>
+      {memoIncButton}
+      <CountOutput>{count}</CountOutput>
+      {memoDecButton}
     </div>
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function CountButton({ label, onUpdate, children }) {
+  useCompareProp(onUpdate);
+
+  return (
+    <button type="button" onClick={onUpdate} aria-label={label}>
+      {children}
+    </button>
   );
 }
 
 // eslint-disable-next-line react/prop-types
 function CountOutput({ children }) {
   return <output>{children}</output>;
-}
-
-// eslint-disable-next-line react/prop-types
-function CountButton({ label, onUpdate, children }) {
-  return (
-    <button type="button" onClick={onUpdate} aria-label={label}>
-      {children}
-    </button>
-  );
 }
 
 /* Props ------------------------------------------------------------------- */
